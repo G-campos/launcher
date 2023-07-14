658fff611442072cdca57653eb8432a78296fe29
Gabriel de Campos <gabodecampos@gmail.com>
2023-07-13 22:22:19 -0300

    add img and close app afte 3sec

--- main.go
+++ main.go
@@ -1,35 +1,42 @@
 package main
 
 import (
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/data/binding"
-	"fyne.io/fyne/v2/widget"
+	"fyne.io/fyne"
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/canvas"
+	"fyne.io/fyne/layout"
+	"fyne.io/fyne/widget"
 	"time"
 )
 
 func main() {
-	a := app.New()
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Launcher App")
+	myWindow.Resize(fyne.NewSize(400, 200))
 
-	w := a.NewWindow("Launcher App")
-	w.Resize(fyne.NewSize(400, 200))
+	// Criar uma imagem para a animação
+	image := canvas.NewImageFromFile("./carbuddy.jpg")
 
-	str := binding.NewString()
-	err := str.Set("Initial value")
-	if err != nil {
-		return
-	}
+	// Configurar o widget da imagem
+	image.FillMode = canvas.ImageFillOriginal
 
-	text := widget.NewLabelWithData(str)
-	w.SetContent(text)
+	// Criar um widget de container para posicionar a imagem no centro
+	container := fyne.NewContainerWithLayout(layout.NewCenterLayout(), image)
 
-	go func() {
-		time.Sleep(time.Second * 2)
-		err := str.Set("A new string")
-		if err != nil {
-			return
-		}
-	}()
+	// Adicionar o widget de container à janela
+	myWindow.SetContent(container)
 
-	w.ShowAndRun() // Finally Running our App
+	// Mostrar a janela
+	myWindow.Show()
+
+	// Aguardar um pouco para exibir a animação
+	time.Sleep(time.Second * 3)
+
+	// Remover o widget da imagem
+	myWindow.SetContent(widget.NewLabel("Aplicativo iniciado!"))
+
+	time.AfterFunc(3*time.Second, myWindow.Close)
+
+	myApp.Run()
+
 }
